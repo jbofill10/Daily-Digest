3133ac6c19d333ab49a01d9cc50d85ea9f45fbc4
Juan Bofill <juan.bofill10@gmail.com>
2019-10-05 21:45:36 -0400

    Set up frontend folder + work on home button component

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomeButton from './components/HomeButton'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomeButton/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HomeButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './CSS/HomeButton.css'
+
+function HomeButton() {
+  return (
+    <div className="HomeButton">
+        <div className='SemiCircle'>
+            <div className='The'>
+                The
+            </div>
+            <div className='DailyDigest'>
+                Daily Digest
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default HomeButton;
