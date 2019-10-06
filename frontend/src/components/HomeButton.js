f68073f6b18a117984a606bb160ea54dd6729c6d
Juan Bofill <juan.bofill10@gmail.com>
2019-10-06 15:13:55 -0400

    Add toolbar

--- frontend/src/components/HomeButton.js
+++ frontend/src/components/HomeButton.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './CSS/HomeButton.css'
+import ToolBar from './ToolBar';
 
 function HomeButton() {
   return (
     <div className="HomeButton">
-        <div className='ToolbarLine'>
-            
-        </div>
-        <div className='SemiCircle'>
-            <div className='The'>
-                The
-            </div>
-            <div className='DailyDigest'>
-                Daily Digest
-            </div>
+      <div className='HeaderContainer'>
+        <ToolBar/>
+          <div className='SemiCircle'>
+              <div className='The'>
+                  The
+              </div>
+              <div className='DailyDigest'>
+                  Daily Digest
+              </div>
+          </div>
         </div>
     </div>
   );
---
+++ frontend/src/components/ToolBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CSS/ToolBar.css'
+
+
+function ToolBar(){
+    return(
+        <div className='Toolbar'>
+
+        </div>
+    );
+}
+
+export default ToolBar;
